6c49d086b3841330080746bcb3d111dc2b9881cc
Akhil <worldincontrol@gmail.com>
2019-06-09 13:01:00 +0530

    Updated createData
    Changed the loop

--- dataCreator.py
+++ dataCreator.py
@@ -10,7 +10,7 @@ for directory, subdirectories, files in os.walk(root): # Parse each  directory i
     for file in files: # Parse each file in that directory
         im = imread(os.path.join(directory,file)) # read the  file and extract its features
         value = im.flatten()
-        value = np.hstack((directory[11:],value))  #The directory[11:] indicates after which character the class name should start so chnage it according to the file name. Hstack places array in stack on each other and directory name is made first coloumn
+        value = np.hstack((directory[8:],value))  #The directory[11:] indicates after which character the class name should start so chnage it according to the file name. Hstack places array in stack on each other and directory name is made first coloumn
         df = pd.DataFrame(value).T
         df = df.sample(frac=1)
         with open('data.csv', 'a') as dataset:
